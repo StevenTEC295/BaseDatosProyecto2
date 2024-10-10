b30a7547c3bdb4676f9896cb99abbd21abb368b1
StevenTEC295 <s.perez.3@estudiantec.cr>
2024-10-10 17:44:32 -0600

    Mejoré el DELETE

--- StoreDataManager/Store.cs
+++ StoreDataManager/Store.cs
@@ -220,6 +220,8 @@ namespace StoreDataManager
         // Función para evaluar las condiciones de la cláusula WHERE
         private bool EvaluateCondition(string dataType, string columnValue, string conditionValue, string operatorValue)
         {
+           
+                
             switch (dataType)
             {
                 case "INTEGER":
@@ -236,15 +238,17 @@ namespace StoreDataManager
                     DateTime dateTimeColumnValue = DateTime.Parse(columnValue);
                     DateTime dateTimeConditionValue = DateTime.Parse(conditionValue);
                     return CompareValues(dateTimeColumnValue, dateTimeConditionValue, operatorValue);
+                default:
 
-                default: // Para strings y otros tipos
             // Aqui se maneja el like
             if (operatorValue == "like")
             {
                 return columnValue.Contains(conditionValue.Replace("*", ""), StringComparison.OrdinalIgnoreCase);
             }
+            else
+                {return CompareValues(columnValue, conditionValue, operatorValue);}
+                
             
-            return CompareValues(columnValue, conditionValue, operatorValue);
         }}
 
          // Función genérica para comparar valores con el operador dado
@@ -396,169 +400,186 @@ namespace StoreDataManager
             return CreateIndexesStoreOperation.Execute(indexName, tableName, columnName, indexType);
         }
 
-        public (OperationStatus Status, string Data) DeleteWhere(string tableName, string columnName, string conditionValue)
+        public (OperationStatus Status, string Data) DeleteWhere(string tableName, string columnName = null, string conditionValue = null)
+        {
+            // Genera la ruta de la tabla donde ejecutar los cambios
+            string fullPath = Path.Combine(RutaDeterminadaPorSet, tableName + ".Table");
+
+            if (!File.Exists(fullPath))
             {
-                //Genera la ruta de la tabla donde ejecutar los cambios
-                string fullPath = Path.Combine(RutaDeterminadaPorSet, tableName + ".Table");
+                return (OperationStatus.Error, $"Error: La tabla '{tableName}' no existe.");
+            }
 
-                if (!File.Exists(fullPath))
+            // Crea una tabla temporal para almacenar los datos (si es necesario)
+            string tempFilePath = Path.Combine(RutaDeterminadaPorSet, tableName + "_temp.Table");
+
+            try
+            {
+                // Abrir el archivo original y el archivo temporal
+                using (FileStream stream = File.Open(fullPath, FileMode.Open))
+                using (BinaryReader reader = new BinaryReader(stream))
+                using (FileStream tempStream = File.Open(tempFilePath, FileMode.Create))
+                using (BinaryWriter writer = new BinaryWriter(tempStream))
                 {
-                    return (OperationStatus.Error, $"Error: La tabla '{tableName}' no existe.");
-                }
-
-                //Crea una tabla temporal para almacenar los datos sin las filas que coincidan con la condición
-                string tempFilePath = Path.Combine(RutaDeterminadaPorSet, tableName + "_temp.Table");
-
-                try
-                {
-                    // Abrir el archivo original y el archivo temporal
-                    using (FileStream stream = File.Open(fullPath, FileMode.Open))
-                    using (BinaryReader reader = new BinaryReader(stream))
-                    using (FileStream tempStream = File.Open(tempFilePath, FileMode.Create))
-                    using (BinaryWriter writer = new BinaryWriter(tempStream))
+                    // Leer encabezado del archivo original
+                    string startMarker = reader.ReadString();
+                    if (startMarker != "TINYSQLSTART")
                     {
-                        // Leer encabezado del archivo original
-                        string startMarker = reader.ReadString();
-                        if (startMarker != "TINYSQLSTART")
-                        {
-                            throw new InvalidDataException("Formato de archivo inválido.");
-                        }
+                        throw new InvalidDataException("Formato de archivo inválido.");
+                    }
 
-                        int columnCount = reader.ReadInt32();
-                        // Leer la estructura de la tabla
-                        List<ColumnDefinition> columns = new List<ColumnDefinition>();
+                    int columnCount = reader.ReadInt32();
+                    // Leer la estructura de la tabla
+                    List<ColumnDefinition> columns = new List<ColumnDefinition>();
+
+                    for (int i = 0; i < columnCount; i++)
+                    {
+                        var column = new ColumnDefinition
+                        {
+                            Name = reader.ReadString(),
+                            DataType = reader.ReadString(),
+                            IsNullable = reader.ReadBoolean(),
+                            IsPrimaryKey = reader.ReadBoolean(),
+                            VarcharLength = reader.ReadInt32()
+                        };
+                        columns.Add(column);
+                    }
+
+                    // Verificar la marca de fin de estructura
+                    string endStructureMarker = reader.ReadString();
+                    if (endStructureMarker != "ENDSTRUCTURE")
+                    {
+                        throw new InvalidDataException("Invalid file structure");
+                    }
+
+                    // Escribir encabezado al archivo temporal
+                    writer.Write("TINYSQLSTART");
+                    writer.Write(columnCount);
+                    // Escribir la estructura de la tabla
+                    foreach (var column in columns)
+                    {
+                        writer.Write(column.Name);
+                        writer.Write(column.DataType);
+                        writer.Write(column.IsNullable);
+                        writer.Write(column.IsPrimaryKey);
+                        
+                        // Manejar el valor nullable de VarcharLength
+                        if (column.VarcharLength.HasValue)
+                        {
+                            writer.Write(column.VarcharLength.Value);  // Escribir el valor si no es null
+                        }
+                        else
+                        {
+                            writer.Write(0);  // Puedes decidir un valor predeterminado o lanzar una excepción
+                        }
+                    }
+                    writer.Write("ENDSTRUCTURE");
+
+                    // Buscar el inicio de los datos
+                    string dataStartMarker = reader.ReadString();
+                    if (dataStartMarker != "DATASTART")
+                    {
+                        throw new InvalidDataException("Marca donde comienza la información no encontrada");
+                    }
+
+                    // Escribir el inicio de los datos en el archivo temporal
+                    writer.Write("DATASTART");
+
+                    // Si no se especifica una columna ni una condición, eliminar todos los registros
+                    if (string.IsNullOrEmpty(columnName) && string.IsNullOrEmpty(conditionValue))
+                    {
+                        // No escribimos ningún dato al archivo temporal, eliminando así todas las filas
+                        stream.Close();  // Cerrar los archivos para evitar errores al reemplazar
+                        tempStream.Close();
+
+                        // Reemplazamos el archivo original por el temporal (sin datos)
+                        File.Delete(fullPath);
+                        File.Move(tempFilePath, fullPath);
+
+                        return (OperationStatus.Success, "Todos los registros fueron eliminados correctamente.");
+                    }
+
+                    // En caso de que exista una condición, procedemos a eliminar basados en esa condición
+                    int columnIndex = columns.FindIndex(c => c.Name == columnName);
+                    if (columnIndex == -1)
+                    {
+                        return (OperationStatus.Error, $"Error: La columna '{columnName}' no existe en la tabla '{tableName}'.");
+                    }
+
+                    // Leer los datos y omitir las filas que coincidan con la condición
+                    while (stream.Position < stream.Length)
+                    {
+                        bool matchCondition = false;
+                        long rowStartPosition = stream.Position;  // Guardamos la posición al inicio de la fila
+                        string[] rowData = new string[columnCount];
 
                         for (int i = 0; i < columnCount; i++)
                         {
-                            var column = new ColumnDefinition
+                            switch (columns[i].DataType)
                             {
-                                Name = reader.ReadString(),
-                                DataType = reader.ReadString(),
-                                IsNullable = reader.ReadBoolean(),
-                                IsPrimaryKey = reader.ReadBoolean(),
-                                VarcharLength = reader.ReadInt32()
-                            };
-                            columns.Add(column);
-                        }
-                        // Verificar la marca de fin de estructura
-                        string endStructureMarker = reader.ReadString();
-                        if (endStructureMarker != "ENDSTRUCTURE")
-                        {
-                            throw new InvalidDataException("Invalid file structure");
-                        }
-
-                        // Escribir encabezado al archivo temporal
-                        writer.Write("TINYSQLSTART");
-                        writer.Write(columnCount);
-                        // Escribir la estructura de la tabla
-                        foreach (var column in columns)
-                        {
-                            writer.Write(column.Name);
-                            writer.Write(column.DataType);
-                            writer.Write(column.IsNullable);
-                            writer.Write(column.IsPrimaryKey);
-
-                            // Manejar el valor nullable de VarcharLength
-                            if (column.VarcharLength.HasValue)
-                            {
-                                writer.Write(column.VarcharLength.Value);  // Escribir el valor si no es null
-                            }
-                            else
-                            {
-                                writer.Write(0);  // Puedes decidir un valor predeterminado o lanzar una excepción
+                                case "INTEGER":
+                                    rowData[i] = reader.ReadInt32().ToString();
+                                    break;
+                                case "DOUBLE":
+                                    rowData[i] = reader.ReadDouble().ToString();
+                                    break;
+                                case "DATETIME":
+                                    long ticks = reader.ReadInt64();
+                                    DateTime dateTime = new DateTime(ticks);
+                                    rowData[i] = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+                                    break;
+                                default:
+                                    int length = reader.ReadInt32();
+                                    rowData[i] = new string(reader.ReadChars(length));
+                                    break;
                             }
                         }
-                        writer.Write("ENDSTRUCTURE");
 
-                        // Buscar el inicio de los datos
-                        string dataStartMarker = reader.ReadString();
-                        if (dataStartMarker != "DATASTART")
+                        // Verificamos si la fila cumple con la condición para eliminar
+                        if (rowData[columnIndex] == conditionValue)
                         {
-                            throw new InvalidDataException("Marca donde comienza la información no encontrada");
+                            matchCondition = true;  // Se omite esta fila
                         }
 
-                        // Escribir el inicio de los datos en el archivo temporal
-                        writer.Write("DATASTART");
-
-                        int columnIndex = columns.FindIndex(c => c.Name == columnName);
-                        if (columnIndex == -1)
+                        // Escribimos la fila al archivo temporal solo si no coincide con la condición
+                        if (!matchCondition)
                         {
-                            return (OperationStatus.Error, $"Error: La columna '{columnName}' no existe en la tabla '{tableName}'.");
-                        }
-
-                        // Leer los datos y omitir las filas que coincidan con la condición
-                        while (stream.Position < stream.Length)
-                        {
-                            bool matchCondition = false;
-                            long rowStartPosition = stream.Position;  // Guardamos la posición al inicio de la fila
-                            string[] rowData = new string[columnCount];
-
                             for (int i = 0; i < columnCount; i++)
                             {
                                 switch (columns[i].DataType)
                                 {
                                     case "INTEGER":
-                                        rowData[i] = reader.ReadInt32().ToString();
+                                        writer.Write(int.Parse(rowData[i]));
                                         break;
                                     case "DOUBLE":
-                                        rowData[i] = reader.ReadDouble().ToString();
+                                        writer.Write(double.Parse(rowData[i]));
                                         break;
                                     case "DATETIME":
-                                        long ticks = reader.ReadInt64();
-                                        DateTime dateTime = new DateTime(ticks);
-                                        rowData[i] = dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+                                        writer.Write(DateTime.Parse(rowData[i]).Ticks);
                                         break;
                                     default:
-                                        int length = reader.ReadInt32();
-                                        rowData[i] = new string(reader.ReadChars(length));
+                                        writer.Write(rowData[i].Length);
+                                        writer.Write(rowData[i].ToCharArray());
                                         break;
-                                }
-                            }
-
-                            // Verificamos si la fila cumple con la condición para eliminar
-                            if (rowData[columnIndex] == conditionValue)
-                            {
-                                matchCondition = true;  // Se omite esta fila
-                            }
-
-                            // Escribimos la fila al archivo temporal solo si no coincide con la condición
-                            if (!matchCondition)
-                            {
-                                for (int i = 0; i < columnCount; i++)
-                                {
-                                    switch (columns[i].DataType)
-                                    {
-                                        case "INTEGER":
-                                            writer.Write(int.Parse(rowData[i]));
-                                            break;
-                                        case "DOUBLE":
-                                            writer.Write(double.Parse(rowData[i]));
-                                            break;
-                                        case "DATETIME":
-                                            writer.Write(DateTime.Parse(rowData[i]).Ticks);
-                                            break;
-                                        default:
-                                            writer.Write(rowData[i].Length);
-                                            writer.Write(rowData[i].ToCharArray());
-                                            break;
-                                    }
                                 }
                             }
                         }
                     }
-
-                    // Reemplazamos el archivo original por el temporal
-                    File.Delete(fullPath);
-                    File.Move(tempFilePath, fullPath);
-
-                    return (OperationStatus.Success, "Filas eliminadas correctamente.");
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error during delete: {ex.Message}");
-                    return (OperationStatus.Error, ex.Message);
-                }
+
+                // Reemplazamos el archivo original por el temporal
+                File.Delete(fullPath);
+                File.Move(tempFilePath, fullPath);
+
+                return (OperationStatus.Success, "Filas eliminadas correctamente.");
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error during delete: {ex.Message}");
+                return (OperationStatus.Error, ex.Message);
+            }
+        }
+
 
 
         
